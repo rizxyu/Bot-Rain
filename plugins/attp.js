7ec5f33af36bded48a20133e4fef61fc821d7b88
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-02 22:19:29 +0700

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,7 +1,15 @@
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
 let handler = async (m, { conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
-  m.reply(`lagi dikirim!\n*jangan lupa donasi Ya,Agar botnya online terus*`)
+  m.reply(`Tunggu sebentar ya`)
+  let stiker = await sticker(null, global.API('xteam', '/attp', { file: '', text: teks }), global.packname, global.author)
+  m.reply(`Await...*`)
+  if (stiker) return conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+  throw stiker.toString()
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
