e0c1dbceffa2bf7170ec2875bb97bdbde649e56b
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-07 10:48:17 +0700

    Add files via upload

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,53 +1,31 @@
 let fetch = require('node-fetch')
 
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/fb', {
-    url: args[0]
-  }, 'APIKEY'))
-  if (res.status !== 200) {
-    res.text()
-    throw res.status
-  }
-  let json = await res.json()
-  if (!json.result) throw json
-  let { name, author, description, uploadDate, duration, url, isFamilyFriendly, genre, keywords, contentSize, videoQuality, commentCount } = json.result
-  let { name: authorname, url: authorlink } = author
-  let dateConfig = {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  }
-  let unknown = '_Unknown_'
-  let none = '_None_'
-  let caption = `
-Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
-Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
-Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
-Size: ${contentSize || unknown}
-Durasi: ${clockString(+ new Date(duration))}
-Genre: ${genre || none}
-Kualitas: ${quality ? quality : unknown}
+let handler = async(m, { conn, text }) => {
 
-${description}
+let apikey = 'HIRO'
 
-Keyword: ${keywords || none}
-`.trim()
-  conn.sendFile(m.chat, url, 'media-fb', caption, m)
+if (!text) throw 'Uhmm Where Url Link?'
+
+let res = await fetch(`https://api.lolhuman.xyz/api/facebook?apikey=${apikey}&url=${text}`)
+let json = await json.res()
+if (json.result) throw 'eror'
+
+conn.sendFile( m.chat, json.result, `link: ${text}\nUrl: ${json.result}`, m)
 }
-handler.help = ['fb'].map(v => v + ' <url>')
+handler.help = ['fb <url>']
 handler.tags = ['downloader']
+handler.command = /^fb$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = true
 
 module.exports = handler
-
-function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
